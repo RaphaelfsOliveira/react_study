7b644e95675a77b4eafc43ee87abb9392de6e42c
Raphael <raphael.oliveira@brilliantmachine.com.br>
2019-03-28 00:56:03 -0300

    config server

---
+++ exercicios/exe_apps/todo-app/backend/config/server.js
@@ -0,0 +1,17 @@
+// IMPORTS
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Server Init
+const app = express();
+const port = process.env.PORT || 3003;
+
+
+// Config Middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.listen(port, () => {
+  console.log(`Backend RESTFull API is running on port ${port}`);
+});
