ceccf9ebae67922cd51430b59949ea510fb554fb
Raphael <raphael.oliveira@brilliantmachine.com.br>
2019-03-30 05:43:43 -0300

    removendo configs desnecessarias

--- exercicios/exe_apps/todo-app/backend/loader.js
+++ exercicios/exe_apps/todo-app/backend/loader.js
@@ -1,4 +1,4 @@
 require('./config/server');
-const app = require('./config/database');
-require('./api/routes/todoList')(app);
+require('./config/database');
+require('./api/models/currency');
 require('./api/tasks/currency');
