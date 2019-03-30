f03e351ef2884c872c2b8c9f505ca414b03dbdab
Raphael <raphael.oliveira@brilliantmachine.com.br>
2019-03-29 23:01:05 -0300

    configs para npm server e docker

--- exercicios/exe_apps/todo-app/backend/config/database.js
+++ exercicios/exe_apps/todo-app/backend/config/database.js
@@ -4,4 +4,8 @@ const mongoose = require('mongoose');
 // Config Promise global
 mongoose.Promise = global.Promise;
 
+// npm run
+// module.exports = mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true });
+
+// docker-compose
 module.exports = mongoose.connect('mongodb://mongo:27017/todo', { useNewUrlParser: true });
