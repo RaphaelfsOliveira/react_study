a3223e94f930cb6236c79e8a239c5ababa45f3ca
Raphael Oliveira <raphaelbrf@gmail.com>
2019-04-13 21:30:58 -0300

    ação do Iconbutton

--- exercicios/exe_apps/todo-app/frontend/src/todo/todoForm.jsx
+++ exercicios/exe_apps/todo-app/frontend/src/todo/todoForm.jsx
@@ -4,16 +4,15 @@ import IconButton from '../template/iconButton';
 
 export default props => (
   <div role='form' className='todoForm'>
-
     <Grid cols='10 10 10 10'>
       <input id='description' className='form-control'
         placeholder='Adicione uma tarefa'>
       </input>
     </Grid>
-
     <Grid cols='2 2 2 2'>
-      <IconButton style='primary' icon='plus'></IconButton>
+      <IconButton style='primary' icon='plus'
+        onClick={props.handleAdd}>
+      </IconButton>
     </Grid>
   </div>
-
 );
