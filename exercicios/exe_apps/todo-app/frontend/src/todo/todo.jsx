f5b9303b5622a06e19c6d9802bc8430ac0f4bf46
Raphael Oliveira <raphaelbrf@gmail.com>
2019-04-19 17:02:46 -0300

    Frontend: - botão de deletar OK

--- exercicios/exe_apps/todo-app/frontend/src/todo/todo.jsx
+++ exercicios/exe_apps/todo-app/frontend/src/todo/todo.jsx
@@ -17,17 +17,32 @@ export default class Todo extends Component {
 
     this.handleAdd = this.handleAdd.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+
+    this.refresh();
+  };
+
+  refresh() {
+    axios.get(`${URL}?sort=-createdAt`)
+    .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    .catch(error => console.log(error));
   };
 
   handleAdd() {
     const description = this.state.description;
     axios.post(URL, {description})
-    .then(resp => console.log('funcionou'))
+    .then(resp => this.refresh())
     .catch(error => console.log('error', error));
   };
 
   handleChange(e) {
     this.setState({...this.state, description: e.target.value});
+  };
+
+  handleRemove(todo) {
+    axios.delete(`${URL}/${todo._id}`)
+    .then(resp => this.refresh())
+    .catch(error => console.log(error));
   };
 
   render() {
@@ -38,7 +53,9 @@ export default class Todo extends Component {
           handleAdd={this.handleAdd}
           description={this.state.description}
           handleChange={this.handleChange}/>
-        <TodoList />
+        <TodoList
+          list={this.state.list}
+          handleRemove={this.handleRemove}/>
       </div>
     )
   };
