5fc55a516f777088ba842da3708cb28e78e67a65
Raphael Oliveira <raphaelbrf@gmail.com>
2019-04-23 23:08:20 -0300

    create new file for todoReducer

--- exercicios/exe_apps/todo-app/frontend/src/main/reducers.js
+++ exercicios/exe_apps/todo-app/frontend/src/main/reducers.js
@@ -1,22 +1,8 @@
 import { combineReducers } from 'redux'
+import todoReducer from '../todo/todoReducer'
 
 const rootReducer = combineReducers({
-  todo: () => ({
-    description: 'Ler livro',
-    list: [{
-      _id: 1,
-      description: 'Pagar fatura do cartao',
-      done: true
-    }, {
-      _id: 2,
-      description: 'Reuniao com a equipe as 10',
-      done: false
-    }, {
-      _id: 3,
-      description: 'Consulta medica na terça',
-      done: false
-    }]
-  })
+  todo: todoReducer
 })
 
 export default rootReducer
