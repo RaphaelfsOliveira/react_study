6d150b2d75a83da0cd119138df27e18aca658a1f
Raphael Oliveira <raphaelbrf@gmail.com>
2019-04-28 23:49:46 -0300

    action for remove todo

--- exercicios/exe_apps/todo-app/frontend/src/todo/todoList.jsx
+++ exercicios/exe_apps/todo-app/frontend/src/todo/todoList.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 
 import IconButton from '../template/iconButton'
 import Grid from '../template/grid'
-import { markAsDone, markAsPending } from './todoActions'
+import { markAsDone, markAsPending, remove } from './todoActions'
 
 const TodoList = props => {
 
@@ -26,7 +26,7 @@ const TodoList = props => {
               onClick={() => props.markAsPending(todo)}>
             </IconButton>
             <IconButton style='danger' icon='trash-o' hide={!todo.done}
-              onClick={() => props.handleRemove(todo)}>
+              onClick={() => props.remove(todo)}>
             </IconButton>
           </Grid>
           <br/><br/>
@@ -57,6 +57,7 @@ const mapStateToProps = state => ({list: state.todo.list})
 const mapDispatchToProps = dispatch => bindActionCreators({
   markAsDone,
   markAsPending,
+  remove,
 }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
