5f63d8abb94aa3c3da19bf5af972668991ab987e
Raphael Oliveira <raphaelbrf@gmail.com>
2019-04-30 09:10:00 -0300

    details

--- exercicios/exe_apps/my-money-app/backend/src/config/server.js
+++ exercicios/exe_apps/my-money-app/backend/src/config/server.js
@@ -8,5 +8,5 @@ server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
 server.listen(port, () => {
-  console.log(`Backend is running on port ${port}.`)
+  console.log(`Backend is running on port ${port}`)
 })
