09af5f08848ad1a672a1e8bc8c5695d51aa21560
Raphael Oliveira <raphaelbrf@gmail.com>
2019-04-30 09:28:52 -0300

    Services - return new object on put - run validators in put

--- exercicios/exe_apps/my-money-app/backend/api/billingCycle/billingCycleServices.js
+++ exercicios/exe_apps/my-money-app/backend/api/billingCycle/billingCycleServices.js
@@ -1,1 +1,13 @@
 const BillingCycle = require('./billingCycle')
+
+BillingCycle.methods(['get', 'post', 'put', 'delete'])
+
+// updateOptions:
+// new -> faz a api retornar sempre o objeto atualizado depois
+// de um metodo put
+// runValidators -> roda a validação de campos tambem na
+// atualização, por padrão o mongo só roda as validaçõe
+// de campo na inclusão de um reistro
+BillingCycle.updateOptions({ new: true, runValidators: true })
+
+module.exports = BillingCycle
