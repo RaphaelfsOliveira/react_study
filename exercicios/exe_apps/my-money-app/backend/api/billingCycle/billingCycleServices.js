820e84c63300fd4f015ca4e8469c701dc6699b04
Raphael Oliveira <raphaelbrf@gmail.com>
2019-05-30 21:34:43 -0300

    API - new route to get summary of credit and debit

--- exercicios/exe_apps/my-money-app/backend/api/billingCycle/billingCycleServices.js
+++ exercicios/exe_apps/my-money-app/backend/api/billingCycle/billingCycleServices.js
@@ -20,4 +20,46 @@ BillingCycle.route('count', (req, res, next) => {
   })
 })
 
+BillingCycle.route('summary', (req, res, next) => {
+  // verificar o erro no mongo nessa rota
+  /*
+  {
+    "erros": [
+        {
+            "name": "MongoError",
+            "message": "The 'cursor' option is required, except for aggregate with the explain argument",
+            "ok": 0,
+            "errmsg": "The 'cursor' option is required, except for aggregate with the explain argument",
+            "code": 9,
+            "codeName": "FailedToParse"
+        }
+    ]
+  }
+  */
+  BillingCycle.aggregate({
+    $project: {
+      credit: {$sum: "$credits.value"},
+      debit: {$sum: "$debits.value"}
+    }
+  }, {
+    $group: {
+      _id: null,
+      credit: {$sum: "$credit"},
+      debit: {$sum: "$debit"}
+    }
+  }, {
+    $project: {
+      _id: 0,
+      credit: 1,
+      debit: 1
+    }
+  }, (error, result) => {
+    if (error) {
+      res.status(500).json({erros: [error]})
+    } else {
+      res.json(result[0] || { credit: 0, debit: 0 })
+    }
+  })
+})
+
 module.exports = BillingCycle
