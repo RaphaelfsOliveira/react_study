52e40f5521f66345128c47b18c16ef8e2a438b4c
Raphael Oliveira <raphaelbrf@gmail.com>
2019-06-22 15:18:07 -0300

    API Middleware - queryParser

--- exercicios/exe_apps/my-money-app/backend/src/config/server.js
+++ exercicios/exe_apps/my-money-app/backend/src/config/server.js
@@ -1,13 +1,15 @@
 const port = 3003
 
-const bodyParser = require('body-parser')
 const express = require('express')
-const server = express()
+const bodyParser = require('body-parser')
 const allowCors = require('./cors')
+const queryParser = require('express-query-int')
+const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, () => {
   console.log(`Backend is running on port ${port}`)
